46f4323be210609dd75edfe1f6289644a2e35aa7
Jaber-Ahmed77 <jaber333513@gmail.com>
2025-02-26 03:38:57 +0200

    fix mongo

--- src/app/lib/connectToDb.js
+++ src/app/lib/connectToDb.js
@@ -4,9 +4,7 @@ let isConnected = false; // Keep track of the connection status
 async function connectToDatabase() {
   if (!isConnected) {
     try {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.MONGO_URI);
       isConnected = true;
       console.log("Database connected");
     } catch (error) {
