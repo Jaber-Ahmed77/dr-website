a39d555f106ba263fa429c41f7c3483d40290b83
Jaber-Ahmed77 <jaber333513@gmail.com>
2025-02-27 18:31:58 +0200

    fix debug

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,14 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import {model, models, Schema} from "mongoose";
 
-const UserSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    image: { type: String },
-    role: { type: String, default: "user" },
-  },
-  { timestamps: true }
-);
+const UserSchema = new Schema({
+  name: String,
+  email: String,
+  image: String,
+  role: { type: String, default: "user" },
+}, {
+  timestamps: true
+});
 
-// Index email for faster lookup (important if you authenticate via email)
 UserSchema.index({ email: 1 });
 
-export default models.User || model("User", UserSchema);
+export const User = models?.User || model('User', UserSchema);
